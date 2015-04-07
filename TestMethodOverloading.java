b04ba609a1dc60c0f813065c69d58b6b6c580dae
Wibby Bernaldi <wibbybernaldi@gmail.com>
2015-04-07 15:05:16 +0700

    Method Overloading

--- TestMethodOverloading.java
+++ TestMethodOverloading.java
@@ -8,11 +8,15 @@ public class TestMethodOverloading {
     public static int average(int n1, int n2, int n3){
         return (n1+n2+n3)/3;
     }
+    public static int average(int n1, int n2, int n3, int n4){
+        return (n1+n2+n3+n4)/2;
+    }
 
     public static void main(String[] args) {
         System.out.println(average(1,2));
         System.out.println(average(1.0,2.0));
         System.out.println(average(1,2,3));
         System.out.println(average(1.0,2));
+        System.out.println(average(1,2,3,4));
     }
 }
